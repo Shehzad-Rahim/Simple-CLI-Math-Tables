2feccf8c3eb2bf7fc151c9496f8c8668462cb630
Shehzad-Rahim <shehzadrahim33@gmail.com>
2024-04-24 15:32:39 +0500

    Maths Table CLI

---
+++ app.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+
+let table = await inquirer.prompt(
+    {
+        name: "num",
+        type: "number",
+        message: "Which table do you want?"
+    }
+);
+
+console.log('**** This is your', table.num, 'Table ****');
+for(let i = 1; i <= 10; i++){
+    
+    console.log(`${table.num} x ${i} = ${table.num * i}`);
+    
+}
